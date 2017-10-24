92ac8ea5919ed5b6bc0bd2bd01f42578cee7c992
Dillon Forestell <djlf729@gmail.com>
2017-10-24 11:19:28 -0500

    make a simple program that generates a random number and then asks the user to guess it. telling the user too high, too low, or just right accordingly

--- Java/PickNumber.java
+++ Java/PickNumber.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class PickNumber {
+
 	public static void main(String[] args) {
-		System.out.println("Set up correctly");
+		Random rand = new Random();
+		int  n = rand.nextInt(10) + 1;
+
+		System.out.println("I picked a number 1-10, can you guess it?");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt(); 
+
+		while (number != n){
+			if (number < 1 || number > 10){
+				System.out.println("That number is not in our range!!! Try again.");
+				number = sc.nextInt();
+			}
+			else if (number > n) {
+				System.out.println("Nope, too high.  Try again");
+				number = sc.nextInt();
+			}
+			else if (number < n) {
+				System.out.println("Nope, too low. Try again");
+				number = sc.nextInt();
+			}
+		}
+
+		System.out.println("You got it my number was " + n );
 	}
 }
