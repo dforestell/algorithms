33a5791bad6c7fcaddab7cfae525f9f5505074de
Dillon Forestell <djlf729@gmail.com>
2017-10-31 14:50:15 -0500

    add a starting age and height of zero to the orangetrees and add a public integer methods to check the age and height of the trees

---
+++ Java/OrangeTree.java
@@ -0,0 +1,22 @@
+class OrangeTree{
+	int age;
+	int height;
+
+	public OrangeTree() {
+		age = 0;
+		height = 0;
+	}
+
+	public int checkAge(){
+		System.out.println("The tree is " + age + " years old.");
+		return age;
+	}
+
+	public int checkHeight(){
+		System.out.println("The tree is " + height + " feet tall.");
+		return height;
+	}
+	public static void main (String[] args){
+
+	}
+}
