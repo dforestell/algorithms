dfbb1c4138e4aa64ac831b550481c966e7cb8c7a
Dillon Forestell <djlf729@gmail.com>
2017-10-31 15:01:05 -0500

    add a boolean oranges variable to the orangetree class and also add a hasOranges boolean method

--- Java/OrangeTree.java
+++ Java/OrangeTree.java
@@ -1,10 +1,12 @@
 class OrangeTree{
 	int age;
 	int height;
+	boolean oranges;
 
 	public OrangeTree() {
 		age = 0;
 		height = 0;
+		oranges = false;
 	}
 
 	public int checkAge(){
@@ -16,6 +18,17 @@ class OrangeTree{
 		System.out.println("The tree is " + height + " feet tall.");
 		return height;
 	}
+
+	public boolean hasOranges(){
+		if (oranges == true){
+			System.out.println("This tree has oranges");
+			return oranges;
+		}
+		else{
+			System.out.println("This tree does not have oranges");
+			return oranges;
+		}
+	} 
 	public static void main (String[] args){
 
 	}
